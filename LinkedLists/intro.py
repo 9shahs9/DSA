a987f02a3e3dc89d4db936c01d973129df3c34f6
Shashank <shashank.bezgoan@gmail.com>
2026-01-21 12:05:42 +0000

    prepend method

--- LinkedLists/intro.py
+++ LinkedLists/intro.py
@@ -64,8 +64,9 @@ class LinkedList:
         if self.length ==0:
             self.head = new_node
             self.tail = new_node
-        new_node.next = self.head
-        self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
         self.length +=1
 
         
